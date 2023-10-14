c533ded9462a4b557b3788c78f490f4c1a58ab72
현기홍 <86722532+GIHng@users.noreply.github.com>
2023-10-14 17:56:13 +0900

    test

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ class Item(BaseModel):
 
 @app.get('/')
 def read_root():
-    return {'hello_havi_1555': 'World'}
+    return {'hello': 'World'}
 
 @app.get('/items/{item_id}')
 def read_item(item_id: int, q: Union[str, None] = None):
