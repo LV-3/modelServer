441022045e1e4310c5b64676020c54b3a0e52300
seunghyuen98 <mexim0905@gamil.com>
2023-12-06 10:57:18 +0900

    [Refactor]: 주석 삭제 및 코드 리펙토링 진행
    - Kafka 관련함수 및 패키지 삭제
    - 각 모델의 코드 전면 리펙토링

--- app/packages/routers/d2v.py
+++ app/packages/routers/d2v.py
@@ -1,5 +1,7 @@
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import pandas as pd
+from app.main import MoodDataItem
+
 
 class Doc2VecModel:
     def __init__(self):
@@ -9,24 +11,26 @@ class Doc2VecModel:
         self.model.train(self.documents, total_examples=self.model.corpus_count, epochs=self.model.epochs)
 
         
-    def tagging_words(self, mood_df) -> TaggedDocument:
+    def tagging_words(self, mood_df: pd.DataFrame) -> TaggedDocument:
         tagged_documents = [TaggedDocument(words = row['extend_template'], tags=[row['title']]) for _, row in mood_df.iterrows()]
 
 
-    def get_similar_movies(self, mood_list) -> list:
+    def get_similar_movies(self, mood_list: list[str]) -> list[str]:
         inferred_vector = self.model.infer_vector(mood_list)
         similar_documents = self.model.dv.most_similar([inferred_vector], topn=21)
         recommended_list = [str(elm[0]) for elm in similar_documents]
+
         return recommended_list
 
 
-    def get_contents_based_rs(self, request_data) -> list: # 변경 예정
+    def get_contents_based_rs(self, request_data: MoodDataItem) -> list[str]:
         md_list = []
+        
         for item in request_data:
             if item.mood is not None:
                 md_list.append(item.mood)
-            # md_list = [item.mood for item in request_data]
         flat_md_list = [item for sublist in md_list for item in sublist]
         
         res_md_list = self.get_similar_movies(flat_md_list)
+
         return res_md_list
